02629c8f9a305e1d538ea4481152c3671b474efd
enidev911 <enidev911@gmail.com>
2024-05-14 11:32:47 -0400

    validate if we have sys.argv

--- actividad-1/imc.py
+++ actividad-1/imc.py
@@ -1,12 +1,5 @@
 import sys
 
-peso = float(sys.argv[1])
-altura = float(sys.argv[2])
-
-altura = altura / 100
-
-nivel_de_peso = round(peso / (altura * altura), 2)
-
 """
    - Bajo Peso: < 18.5
    - Adecuado: 18.5 - 25
@@ -15,18 +8,28 @@ nivel_de_peso = round(peso / (altura * altura), 2)
    - Obesidad grado II: 35 - 40
    - Obesidad grado III: Más de 40
 """
- 
-clasificacion = "Su IMC es %s\nLa clasificación OMS es %s"
 
-if nivel_de_peso <= 18.5:
-    print(clasificacion % (nivel_de_peso, "Bajo Peso"))
-elif nivel_de_peso <= 25:
-    print(clasificacion % (nivel_de_peso, "Adecuado"))
-elif nivel_de_peso <= 30:
-    print(clasificacion % (nivel_de_peso, "Sobrepeso"))
-elif nivel_de_peso <= 35:
-    print(clasificacion % (nivel_de_peso, "Obesidad Grado I"))
-elif nivel_de_peso <= 40:
-    print(clasificacion % (nivel_de_peso, "Obesidad Grado II"))
+if len(sys.argv) == 3:
+
+    peso = float(sys.argv[1])
+    altura = float(sys.argv[2]) / 100
+
+    nivel_de_peso = round(peso / (altura * altura), 2)
+    clasificacion = "Su IMC es %s\nLa clasificación OMS es %s"
+
+    if nivel_de_peso <= 18.5:
+        print(clasificacion % (nivel_de_peso, "Bajo Peso"))
+    elif nivel_de_peso <= 25:
+        print(clasificacion % (nivel_de_peso, "Adecuado"))
+    elif nivel_de_peso <= 30:
+        print(clasificacion % (nivel_de_peso, "Sobrepeso"))
+    elif nivel_de_peso <= 35:
+        print(clasificacion % (nivel_de_peso, "Obesidad Grado I"))
+    elif nivel_de_peso <= 40:
+        print(clasificacion % (nivel_de_peso, "Obesidad Grado II"))
+    else:
+        print(clasificacion % (nivel_de_peso, "Obesidad Grado III"))
+
 else:
-    print(clasificacion % (nivel_de_peso, "Obesidad Grado III"))
+    msg = "Debes pasarme los argumentos peso y altura por línea de comandos"
+    print(msg + "\nEj: python imc.py 81 178")
